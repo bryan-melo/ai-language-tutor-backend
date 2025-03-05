9f36395e2f2b56246f428bcc450d644315508c8c
Bryan Melo <bmelo650@gmail.com>
2025-03-04 18:32:37 -0800

    Schema for account created

---
+++ backend/app/schemas/account.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+
+class AccountBase(BaseModel):
+   f_name: str
+   l_name: str
+   email: str
+   username: str
+   password: str
+   primary_lang: str
+   
+   
+class AccountCreate(AccountBase):
+   pass
+
+
+class Account(AccountBase):
+   id: int
+   
+   class Config:
+      org_mode = True
+      
