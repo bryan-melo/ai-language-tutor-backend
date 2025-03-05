45cd40e93d7c76e69a51fb160370f46bff88de87
Bryan Melo <bmelo650@gmail.com>
2025-03-04 18:49:00 -0800

    Updated main.py

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 # Define allowed origins for Cross-Origin Resource Sharing (CORS)
 origins = [
-    "http://localhost:3000/courses"     # Allow requests from frontend running on localhost:3000
+    "http://localhost:3000/courses"    
 ]
 
 # Add CORS middleware
