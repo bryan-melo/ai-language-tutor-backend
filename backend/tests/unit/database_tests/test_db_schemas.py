2cb48cae84ea0f663dc25b182822b9de76950946
Bryan Melo <bmelo650@gmail.com>
2025-06-26 02:42:41 -0700

    Created more unit tests for db schema testing

--- backend/tests/unit/database_tests/test_db_schemas.py
+++ backend/tests/unit/database_tests/test_db_schemas.py
@@ -25,9 +25,36 @@ class TestDatabaseSchemas:
          Account.model_validate({
             "f_name": "Bryan",
             "l_name": "Melo",
+            # Missing email field
             "username": "bryan123",
             "password": "1234",
             "primary_lang": "en"
+         })
+         
+         
+   def test_course_schema_valid(self):
+      course = Course(
+         title="Testing Course",
+         author="Bryan",
+         description="This is a test",
+         num_of_lessons=100,
+         category="Testing",
+         difficulty="Expert"
+      )
+      
+      assert course.author == "Bryan"
+      assert course.num_of_lessons == 100
+      
+   
+   def test_course_schema_missing_required(self):
+      with pytest.raises(ValidationError):
+         Course.model_validate({
+            "title": "Testing Course",
+            "author": "Bryan",
+            # Missing description field
+            "num_of_lessons": 100,
+            "category": "Testing",
+            "difficulty": "Expert"
          })
       
       
