90f18b1c2afb8c277fc4e07fffba8cdc450a2e3f
Bryan Melo <bmelo650@gmail.com>
2025-06-26 20:13:39 -0700

    Database schema unit testing completed

--- backend/tests/unit/database_tests/test_db_schemas.py
+++ backend/tests/unit/database_tests/test_db_schemas.py
@@ -19,19 +19,20 @@ class TestDatabaseSchemas:
       assert account.email == "bryan@test.com" 
       
    
-   # Test Account schema for invalid instance
+   # Test Account schema for invalid instance -- missing field
    def test_account_schema_missing_required(self):
       with pytest.raises(ValidationError):
          Account.model_validate({
             "f_name": "Bryan",
             "l_name": "Melo",
-            # Missing email field
+            # Missing 'email' field
             "username": "bryan123",
             "password": "1234",
             "primary_lang": "en"
          })
          
-         
+   
+   # Test Course schema for valid instance
    def test_course_schema_valid(self):
       course = Course(
          title="Testing Course",
@@ -46,15 +47,63 @@ class TestDatabaseSchemas:
       assert course.num_of_lessons == 100
       
    
+   # Test Course schema for invalid instance -- missing field
    def test_course_schema_missing_required(self):
       with pytest.raises(ValidationError):
          Course.model_validate({
             "title": "Testing Course",
             "author": "Bryan",
-            # Missing description field
+            # Missing 'description' field
             "num_of_lessons": 100,
             "category": "Testing",
             "difficulty": "Expert"
          })
+         
+   
+   # Test Course schema for invalid instance -- atribute error
+   def test_course_schema_incorrect_type(self):
+      with pytest.raises(ValidationError):
+         Course.model_validate({
+            "title": "Test Course",
+            "author": "Bryan",
+            "description": "Testing Course",
+            "num_of_lessons": "one hundred",   # Incorrect data type
+            "category": 100,
+            "difficulty": "Expert"
+         })
+
+
+   # Test Lesson schema for valid instance
+   def test_lesson_schema_valid(self):
+      lesson = Lesson(
+         title="Testing Lesson",
+         lesson_num=2,
+         material=["Test 1", "Test 2", "Test 3"],
+         parent_course=1
+      )
       
-      
+      assert lesson.title == "Testing Lesson"
+      assert lesson.parent_course == 1
+   
+   
+   # Test Lesson schema for invalid instance -- missing field
+   def test_lesson_schema_required(self):
+      with pytest.raises(ValidationError):
+         Lesson.model_validate({
+            "title": "Testing Lesson 2",
+            "lesson_num": 3,
+            # Missing 'material' field
+            "parent_course": 2
+         })
+         
+   
+   # Test Lesson schema for invalid instance -- attribute error
+   def test_lesson_schema_incorrect_type(self):
+      with pytest.raises(ValidationError):
+         Lesson.model_validate({
+            "title": "Testing Lesson 3",
+            "lesson_num": "three",   # Incorrect data type
+            "material": ["Test 1", "Test 2", "Test 3"],
+            "parent_course": 2
+         })
+   
---
+++ backend/tests/unit/model_tests/test_account_models.py
@@ -0,0 +1,9 @@
+import pytest
+from app.models import *
+
+
+class TestAccountModels:
+   # Test account read model for validation
+   def test_account_read_valid(self):
+      pass
+
