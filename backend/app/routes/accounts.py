0552d8e8bd336f7be7508396dd2f9ac02b286747
Bryan Melo <bmelo650@gmail.com>
2025-06-28 20:12:59 -0700

    Test plan for API testing completed (rough draft)

--- backend/app/routes/accounts.py
+++ backend/app/routes/accounts.py
@@ -31,6 +31,23 @@ def create_account(account: Account, session: SessionDep) -> AccountRead:
     return db_account
 
 
+# Route to login
+@router.post("/login", response_model=AccountRead)
+def login(request: LoginRequest, session: SessionDep) -> AccountRead:
+    account = session.exec(
+        select(Account).where(Account.username == request.username)
+    ).first()
+
+    if not account or not verify_password(request.password, account.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    db_account = session.get(Account, account.id)
+    if not db_account:
+        raise HTTPException(status_code=404, detail="Account not found")
+
+    return db_account
+
+
 # Route to get all accounts in database
 @router.get("/get-all-accounts", response_model=list[AccountRead])
 def get_all_accounts(session: SessionDep) -> list[Account]:
@@ -60,23 +77,6 @@ def delete_account(account_id: int, session: SessionDep):
     session.delete(account)
     session.commit()
     return {"ok": True}
-
-
-# Route to login
-@router.post("/login", response_model=AccountRead)
-def login(request: LoginRequest, session: SessionDep) -> AccountRead:
-    account = session.exec(
-        select(Account).where(Account.username == request.username)
-    ).first()
-
-    if not account or not verify_password(request.password, account.password):
-        raise HTTPException(status_code=401, detail="Invalid credentials")
-
-    db_account = session.get(Account, account.id)
-    if not db_account:
-        raise HTTPException(status_code=404, detail="Account not found")
-
-    return db_account
 
 
 # Route to log out
--- backend/tests/api/README.md
+++ backend/tests/api/README.md
@@ -5,9 +5,17 @@ This file outlines what to expect in the API directory such as documentation for
 ---
 
 ## Table of Contents
-1. [Documentation](#documentation)
-2. [Routes](#routes)
-3. [Examples](#examples)
+1. [Tech Stack](#tech-stack)
+2. [Documentation](#documentation)
+3. [Routes](#routes)
+4. [HTTP Response Status Codes](#http-response-status-codes)
+5. [Examples](#examples)
+6. [Resources](#resources)
+
+---
+
+## Tech Stack
+- List out programming languages, libraries, and frameworks used
 
 ---
 
@@ -15,8 +23,31 @@ This file outlines what to expect in the API directory such as documentation for
 - Briefly explain where documentation can be found
 - Briefly explain what is being documented
 
+---
+
 ## Routes 
 - Go over the routes that are being tested
 
+---
+
+## HTTP Response Status Codes
+1. Information responses (100 - 199)
+2. Successful responses (200 - 299)
+3. Redirection messages (300 - 399)
+4. Client error responses (400 - 499)
+5. Server error responses (500 - 599)
+
+---
+
 ## Examples
 - Briefly go over 1-2 examples of API testing
+
+---
+
+## Resources
+- [How to Write a QA Test Plan](https://testlio.com/blog/write-qa-test-plan/)
+- [pytest how-to guides](https://docs.pytest.org/en/stable/how-to/index.html)
+- [HTTP response status codes](https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status)
+- [Send responses to HTTPX using pytest](https://pypi.org/project/pytest-httpx/)
+- [HTTP modules](https://docs.python.org/3/library/http.html)
+- [Produce coverage reports](https://pypi.org/project/pytest-cov/)
