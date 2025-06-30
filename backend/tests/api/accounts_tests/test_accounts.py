2de42b64e2f553c9462f398ee925a43351115511
Bryan Melo <bmelo650@gmail.com>
2025-06-29 23:53:07 -0700

    Removed fixture (not needed)

--- backend/tests/api/accounts_tests/test_accounts.py
+++ backend/tests/api/accounts_tests/test_accounts.py
@@ -7,12 +7,6 @@ def get_data():
    return {"username": "test"}
 
 
-@pytest.fixture
-def db_connection():
-   conn = connect_to_db()
-   yield conn
-   conn.close()
-
 def test_example(get_data):
    assert "username" in get_data
    
