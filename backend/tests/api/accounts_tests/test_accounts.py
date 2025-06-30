3bd58c91bb8f09d5822fc93d3d9df0948002fe86
Bryan Melo <bmelo650@gmail.com>
2025-06-29 23:54:19 -0700

    Database connection fixture added

--- backend/tests/api/accounts_tests/test_accounts.py
+++ backend/tests/api/accounts_tests/test_accounts.py
@@ -11,3 +11,11 @@ def test_example(get_data):
    assert "username" in get_data
    
 
+@pytest.fixture
+def config():
+   return {"db": "sqlite"}
+
+
+@pytest.fixture
+def db_connection(config):
+   return connect(config["db"])
