70847d551170e2f07739f1d57f68672913e304c2
Bryan Melo <bmelo650@gmail.com>
2025-06-30 00:44:59 -0700

    Removed some tests

--- backend/tests/api/accounts_tests/test_accounts.py
+++ backend/tests/api/accounts_tests/test_accounts.py
@@ -10,15 +10,3 @@ def get_data():
 def test_example(get_data):
    assert "username" in get_data
    
-
-@pytest.fixture
-def config():
-   return {"db": "sqlite"}
-
-
-@pytest.fixture
-def db_connection(config):
-   return connect(config["db"])
-
-
-assert "username" in get_data()
