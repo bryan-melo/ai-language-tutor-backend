9f8505e6c912baca3b90c1d4861a42fbae735492
Bryan Melo <bmelo650@gmail.com>
2025-07-08 19:41:06 -0700

    Added test case

--- backend/tests/api/accounts_tests/test_accounts.py
+++ backend/tests/api/accounts_tests/test_accounts.py
@@ -3,10 +3,10 @@ import httpx
 
 
 @pytest.fixture
-def get_data():
+def get_data_example():
    return {"username": "test"}
 
 
 def test_example(get_data):
-   assert "username" in get_data
+   assert "username" in get_data_example
 
