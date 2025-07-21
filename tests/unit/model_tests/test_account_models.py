be09ab5a9c61e713a437f15a9b58b27522042002
Bryan Melo <bmelo650@gmail.com>
2025-07-21 15:53:21 -0700

    Made changes to unit test to reflect supported languages

--- tests/unit/model_tests/test_account_models.py
+++ tests/unit/model_tests/test_account_models.py
@@ -12,7 +12,7 @@ class TestAccountModels:
          "l_name": "Melo",
          "email": "bryan@test.com",
          "username": "Tester",
-         "primary_lang": "English"
+         "primary_lang": "english"
       }
       model = AccountRead(**data)
       
@@ -29,7 +29,7 @@ class TestAccountModels:
             # Missing 'l_name' field
             "email": "bryan@test.com",
             "username": "Tester",
-            "primary_lang": "English"
+            "primary_lang": "english"
       })
          
    
@@ -42,7 +42,7 @@ class TestAccountModels:
             "l_name": "Melo",
             "email": "bryan@test.com",
             "username": "Tester",
-            "primary_lang": "English"
+            "primary_lang": "english"
          })
          
    
